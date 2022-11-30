88b4e6faf37d2edd6797604ed6a3174b8920f06c
ErJump <fedelijunior@gmail.com>
2022-11-30 17:25:05 +0100

    feat: :sparkles: Adds query to get all nations

--- src/main/java/org/generation/italy/Main.java
+++ src/main/java/org/generation/italy/Main.java
@@ -1,7 +1,41 @@
 package org.generation.italy;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class Main {
+	
+	private static final String URL = "jdbc:mysql://localhost:3306/nations";
+	private static final String USER = "root";
+	private static final String PASSWORD = "root";
+	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		try (Connection con = DriverManager.getConnection(URL, USER, PASSWORD)){
+			
+			 String sql = "SELECT countries.name, countries.country_id, regions.name, continents.name"
+					 + " FROM countries "
+					 + " JOIN regions "
+					 + " ON countries.region_id = regions.region_id "
+					 + " JOIN continents "
+					 + " ON regions.continent_id = continents.continent_id "
+					 + " ORDER BY countries.name ";
+			
+			try (PreparedStatement ps = con.prepareStatement(sql)) {
+				try (ResultSet rs = ps.executeQuery()) {
+					while(rs.next()) {
+						final String name = rs.getString(1);
+						final int id = rs.getInt(2);
+						final String regionName = rs.getString(3);
+						final String continentName = rs.getString(4);
+						
+						System.out.println(id + " - " + name + " - " + regionName + " - " + continentName);
+					}
+				}
+			}
+		} catch (Exception e) {
+			System.err.println("Error: " + e.getMessage());
+		}
 	}
 }
